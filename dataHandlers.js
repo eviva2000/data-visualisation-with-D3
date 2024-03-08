1d5d39fb337c7a63eb89c1c10b40fc1468b5baad
eviva2000 <arjomandielaheh@gamin.com>
2024-03-08 12:13:18 +0100

    added different data format handling

---
+++ dataHandlers.js
@@ -0,0 +1,39 @@
+let parseDate = d3.timeParse("%m/%d/%Y");
+
+// csv data handler
+d3.csv("prices.csv")
+  .row(function (d) {
+    return {
+      month: parseDate(d.month),
+      price: Number(d.price.trim().slice(1)),
+    };
+  })
+  .get(function (error, data) {});
+
+// tsv data handler
+d3.tsv("data.tsv")
+  .row(function (d) {
+    return {
+      month: parseDate(d.month),
+      price: Number(d.price.trim().slice(1)),
+    };
+  })
+  .get(function (error, data) {
+    console.log(data);
+  });
+
+// txt (dsv) data handler
+const psv = d3.dsvFormat("|");
+
+d3.text("data.txt").get(function (error, data) {
+  const rows = psv.parse(data);
+  const newRows = [];
+
+  for (let p = 0; p < rows.length; p++) {
+    newRows.push({
+      month: parseDate(rows[p].month),
+      price: Number(rows[p].price.trim().slice(1)),
+    });
+  }
+  console.log("rows", newRows);
+});
